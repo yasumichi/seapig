34f88670a8e701e25446b12722aa98d9ec03cc67
Yasumichi Akahoshi <yasumichi@vinelinux.org>
2016-08-20 17:04:14 +0900

    fixes #11 , change theme from monokai to twilight

--- js/renderer.js
+++ js/renderer.js
@@ -9,14 +9,14 @@ var currentFile = "";
 
 // Initialize ace editor
 require('ace-min-noconflict');
-require('ace-min-noconflict/theme-monokai');
+require('ace-min-noconflict/theme-twilight');
 require('ace-min-noconflict/mode-markdown');
 require('ace-min-noconflict/keybinding-emacs');
 require('ace-min-noconflict/keybinding-vim');
 var editor = null;
 window.addEventListener('DOMContentLoaded',function() {
   editor = ace.edit("aceEditor");
-  editor.setTheme("ace/theme/monokai");
+  editor.setTheme("ace/theme/twilight");
   editor.getSession().setMode("ace/mode/markdown");
   editor.getSession().setUseWrapMode(true);
   editor.focus();
