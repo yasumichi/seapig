dcf7ebc7fef9b2ed0df9cd4c7eb9539d220af946
Yasumichi Akahoshi <yasumichi@vinelinux.org>
2016-08-27 21:17:54 +0900

    add property to dot-location of eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,7 +36,10 @@ module.exports = {
         "consistent-this": "error",
         "curly": "off",
         "default-case": "error",
-        "dot-location": "error",
+        "dot-location": [
+          "error",
+          "property"
+        ],
         "dot-notation": "error",
         "eol-last": "error",
         "eqeqeq": "off",
