2ce3f240c219ef71b12684855c2f704a9440c906
Yasumichi Akahoshi <sekiseijuju@gmail.com>
2019-09-14 10:40:04 +0900

    update logic for other external library.

--- scripts/build-dep.js
+++ scripts/build-dep.js
@@ -2,19 +2,31 @@ const fs = require('fs');
 const path = require('path');
 
 const extdir = "external";
-const acedest = path.join(extdir, "ace");
-const srcfiles = [
-    'ace-builds/src-min-noconflict/ace.js',
-    'ace-builds/src-min-noconflict/theme-twilight.js',
-    'ace-builds/src-min-noconflict/mode-markdown.js',
-    'ace-builds/src-min-noconflict/keybinding-emacs.js',
-    'ace-builds/src-min-noconflict/keybinding-vim.js',
+
+const copyplan = [
+	// for ace
+	{
+		destdir: path.join(extdir, "ace"),
+		srcfiles: [
+			'ace-builds/LICENSE',
+			'ace-builds/src-min-noconflict/ace.js',
+			'ace-builds/src-min-noconflict/keybinding-emacs.js',
+			'ace-builds/src-min-noconflict/keybinding-vim.js',
+			'ace-builds/src-min-noconflict/mode-markdown.js',
+			'ace-builds/src-min-noconflict/theme-twilight.js',
+		]
+	},
 ];
 
-if ( fs.existsSync(acedest) == false ) {
-    fs.mkdirSync(acedest, {recursive: true});
+copyplan.forEach((value) => {
+  var destdir = value.destdir;
+  var srcfiles = value.srcfiles;
+
+  if ( fs.existsSync(destdir) == false ) {
+    fs.mkdirSync(destdir, {recursive: true});
     srcfiles.forEach((value) => {
-        var destfile = path.join(acedest, path.basename(value));
-        fs.copyFileSync(value, destfile);
+      var destfile = path.join(destdir, path.basename(value));
+      fs.copyFileSync(value, destfile);
     });
-}
+  }
+});
