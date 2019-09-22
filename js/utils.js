8bd226fa95984de6e0b7fedea27d7cb30a980d75
Yasumichi Akahoshi <sekiseijuju@gmail.com>
2019-09-22 17:33:44 +0900

    add milliseconds to mermaid id ( close #40 )

--- js/utils.js
+++ js/utils.js
@@ -44,7 +44,8 @@ const mermaidAPI = require('../external/mermaid/mermaid.min.js').mermaidAPI;
       let date = new Date();
       let svgId = "m" + date.getFullYear() + ('0' + (date.getMonth() + 1)).slice(-2)
         + ('0' + date.getDate()).slice(-2) + ('0' + date.getHours()).slice(-2)
-        + ('0' + date.getMinutes()).slice(-2) + ('0' + date.getSeconds()).slice(-2);
+        + ('0' + date.getMinutes()).slice(-2) + ('0' + date.getSeconds()).slice(-2)
+        + ('00' + date.getMilliseconds()).slice(-3);
       let svg = mermaidAPI.render(svgId, code);
 
       return  `<div class="mermaid">\n${svg}</div>\n`;
